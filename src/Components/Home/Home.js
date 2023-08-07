519e4c02d3ba7991f0f2998b2dbf1d364f25d7bb
Mahlatse <millieniummorapedi0212@gmail.com>
2023-08-07 16:36:16 +0200

    Added an image to the view

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Home.scss'
+import tshirt from '../../images/tshirt.png'
 
 function Home() {
   return (
-    <div></div>
+    <div className='home'>
+      <div className='img_block'>
+        <img src={ tshirt } alt="T shirt" />
+      </div>
+    </div>
   )
 }
 
